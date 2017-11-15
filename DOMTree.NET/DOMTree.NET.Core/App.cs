0d77563a93b5cd48a4436a9da302e3b7c747f294
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-15 17:55:09 +0100

    Prepare DOMTree.NET.Core Project for development

---
+++ DOMTree.NET/DOMTree.NET.Core/App.cs
@@ -0,0 +1,32 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMTree.NET.Core
+{
+    public class App : MvxApplication
+    {
+
+        /// <summary>
+        /// Setup IoC registrations.
+        /// </summary>
+        public App()
+        {
+            // Whenever Mvx.Resolve is used, a new instance of Calculation is provided.
+           // Mvx.RegisterType<IBillCalculator, BillCalculator>();
+          //  var calcExample = Mvx.Resolve<IBillCalculator>();
+
+            // Tells the MvvmCross framework that whenever any code requests an IMvxAppStart reference,
+            // the framework should return that same appStart instance.
+            var appStart = new CustomAppStart();
+            Mvx.RegisterSingleton<IMvxAppStart>(appStart);
+
+            // Another option is to utilize a default App Start object with 
+            // var appStart = new MvxAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ DOMTree.NET/DOMTree.NET.Core/CustomAppStart.cs
@@ -0,0 +1,27 @@
+﻿using DOMTree.NET.Core.ViewModels;
+using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMTree.NET.Core
+{
+    /// <summary>
+    /// This class is used to customize how the application starts
+    /// and which view is loaded on start.
+    /// </summary>
+    class CustomAppStart : MvxNavigatingObject, IMvxAppStart
+    {
+        /// <summary>
+        /// Hint can take command-line startup parameters, and then pass them to the called view models.
+        /// </summary>
+        public void Start(object hint = null)
+        {
+            // ShowViewModel is a core navigation mechanism in MvvmCross.
+            // for now, just start the regular MainMenuViewModel view.
+            ShowViewModel<MainViewModel>();
+        }
+    }
+}
