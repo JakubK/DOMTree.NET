81e58112a2ec5e8ce6f8409c5dd77c58046ca997
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-15 17:55:36 +0100

    Add empty MainViewModel to DOMTree.NET.Core Project

---
+++ DOMTree.NET/DOMTree.NET.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DOMTree.NET.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+    
+    }
+}
