3b9d10b44c268ded4edb677879a122588aa47be4
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-15 17:54:36 +0100

    Add Startup classes to DOMTree.NET Project

---
+++ DOMTree.NET/DOMTree.NET/Setup.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Wpf.Platform;
+using MvvmCross.Wpf.Views.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace DOMTree.NET
+{
+    public class Setup : MvxWpfSetup
+    {
+        public Setup(Dispatcher uiThreadDispatcher, IMvxWpfViewPresenter presenter) : base(uiThreadDispatcher, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+           // var dbConn = FileAccessHelper.GetLocalFilePath("restaurant.db3");
+            //Mvx.RegisterSingleton(new Repository(dbConn));
+            return new Core.App();
+        }
+    }
+}
