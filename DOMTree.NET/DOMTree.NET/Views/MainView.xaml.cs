9e5f8e4649a22e547613fe109ece33cbf07535ee
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-15 17:55:53 +0100

    Add base MainView to DOMTree.NET Project

---
+++ DOMTree.NET/DOMTree.NET/Views/MainView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DOMTree.NET.Views
+{
+    public partial class MainView : MvxWpfView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
