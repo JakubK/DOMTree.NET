24353b4a04bac71758cb45d745ed4e244dd99039
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-18 21:27:21 +0100

    Add empty DesignView and CodeView to DOMTree.NET (WPF)

---
+++ DOMTree.NET/DOMTree.NET/Views/Code/CodeView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DOMTree.NET.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DOMTree.NET.Views.Code
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CodeView.xaml
+    /// </summary>
+    [MvxRegion("PageContent")]
+    public partial class CodeView : MvxWpfPage
+    {
+        public new CodeViewModel ViewModel
+        {
+            get { return (CodeViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        public CodeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DOMTree.NET/DOMTree.NET/Views/Design/DesignView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DOMTree.NET.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DOMTree.NET.Views.Design
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DesignView.xaml
+    /// </summary>
+    [MvxRegion("PageContent")]
+    public partial class DesignView : MvxWpfPage
+    {
+        public new DesignViewModel ViewModel
+        {
+            get { return (DesignViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+        
+        public DesignView()
+        {
+            InitializeComponent();
+        }
+    }
+}
