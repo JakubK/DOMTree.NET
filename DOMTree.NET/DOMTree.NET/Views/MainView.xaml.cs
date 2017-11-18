e45d6b1c0b9a7c6e91a443469bd48c8876c227db
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-18 21:34:25 +0100

    Add MainView to DOMTree.NET (WPF)

--- DOMTree.NET/DOMTree.NET/Views/MainView.xaml.cs
+++ DOMTree.NET/DOMTree.NET/Views/MainView.xaml.cs
@@ -1,20 +1,29 @@
-﻿using MvvmCross.Wpf.Views;
+﻿using DOMTree.NET.Core.ViewModels;
+using MvvmCross.Wpf.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
 namespace DOMTree.NET.Views
 {
     public partial class MainView : MvxWpfView
     {
+        public new MainViewModel ViewModel
+        {
+            get { return (MainViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private void MainView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ViewModel.LoadViewModel();
         }
     }
 }
