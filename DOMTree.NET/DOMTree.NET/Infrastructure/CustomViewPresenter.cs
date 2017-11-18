78dafd686963edf1d21a2a4f084e6bacfbd3ad37
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-18 21:32:29 +0100

    Add Infrastructure classes to DOMTree.NET (WPF) project

---
+++ DOMTree.NET/DOMTree.NET/Infrastructure/CustomViewPresenter.cs
@@ -0,0 +1,30 @@
+﻿using DOMTree.NET.Views;
+using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DOMTree.NET
+{
+    public class CustomViewPresenter : MvxMultiRegionWpfViewPresenter
+    {
+        ContentControl _contentControl;
+
+        public CustomViewPresenter(ContentControl contentControl)
+            : base(contentControl)
+        {
+            _contentControl = contentControl;
+        }
+
+        public override void ChangePresentation(MvxPresentationHint hint)
+        {
+           
+
+            base.ChangePresentation(hint);
+        }
+    }
+}
---
+++ DOMTree.NET/DOMTree.NET/Infrastructure/MvxWpfPage.cs
@@ -0,0 +1,37 @@
+﻿using MvvmCross.Core.Views;
+using MvvmCross.Platform.Core;
+using MvvmCross.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MvvmCross.Core.ViewModels;
+
+namespace DOMTree.NET
+{
+    public class MvxWpfPage : Page, IMvxWpfView, IMvxView, IMvxDataConsumer
+    {
+        private IMvxViewModel _viewModel;
+        public IMvxViewModel ViewModel
+        {
+            get { return this._viewModel; }
+            set
+            {
+                this._viewModel = value;
+                this.DataContext = value;
+            }
+        }
+    }
+
+    public class MvxWpfPage<TViewModel>
+        : MvxWpfPage, IMvxWpfView<TViewModel> where TViewModel : class, IMvxViewModel
+    {
+        public new TViewModel ViewModel
+        {
+            get { return (TViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+    }
+}
