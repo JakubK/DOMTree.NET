c6209450515db49a1eb976f10dc467ad968c81e6
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-25 13:48:01 +0100

    Create new Service class for logging rendered Views

---
+++ DOMTree.NET/DOMTree.NET.Core/Interfaces/IViewReportService.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMTree.NET.Core.Interfaces
+{
+    public interface IViewReportService<Type>
+    {
+        /// <summary>
+        /// Stores all currently shown views
+        /// </summary>
+        List<Type> CurrentViews { get; set; }
+
+        /// <summary>
+        /// Checks if the view is currently begin rendered
+        /// </summary>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        bool IsLoaded(Type view);
+
+        void AddView(Type view);
+
+        void RemoveView(Type view);
+    }
+}
---
+++ DOMTree.NET/DOMTree.NET.Core/Services/ViewReportService.cs
@@ -0,0 +1,39 @@
+﻿using DOMTree.NET.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.Views;
+using MvvmCross.Core.ViewModels;
+
+namespace DOMTree.NET.Core.Services
+{
+    public class ViewReportService : IViewReportService<Type>
+    {
+        public List<Type> CurrentViews { get; set; }
+
+        public ViewReportService()
+        {
+            if(CurrentViews == null)
+                CurrentViews = new List<Type>();
+        }
+
+        public bool IsLoaded(Type view)
+        {
+            return CurrentViews.Contains(view) ? true : false;
+        }
+
+        public void AddView(Type view)
+        {
+            if(!IsLoaded(view))
+                CurrentViews.Add(view);
+        }
+
+        public void RemoveView(Type view)
+        {
+            if (IsLoaded(view))
+                CurrentViews.Remove(view);
+        }
+    }
+}
